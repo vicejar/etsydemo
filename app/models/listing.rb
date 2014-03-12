40c59a127cd8ff9a3f8a9bca5669882facbd3ee7
Your Actual Name <Your Actual Email>
2014-03-12 10:52:22 -0700

    first Commit

---
+++ app/models/listing.rb
@@ -0,0 +1,4 @@
+class Listing < ActiveRecord::Base
+	#class User < ActiveRecord::Base
+  has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Etsydemo::Application.config.secret_key_base = '494d0432589387d85fecfbc56d0c3b52a8ad9edaf066b01138daf4bac0850306c8563d13be41342a205516b3d1b6c6fd37514b5f18e45d40ee4f03b6a092fa9e'
